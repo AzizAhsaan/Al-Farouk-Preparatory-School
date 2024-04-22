94b5807d6bcdcde2a4bb2a38d9c17bb2ecbd4967
AzizAhsaan <azizahsaan339@gmail.com>
2024-04-22 13:35:48 +0300

    trying to fix the bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import LandingPage from "./Pages/LandingPage";
 import Layout from "./Components/Layout";
-
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 function App() {
 
   return (
     <>
      <BrowserRouter>
+     <Header />
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />}>
         </Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
     </>
   )
